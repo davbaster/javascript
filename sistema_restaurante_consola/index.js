a1e9a1468f9fb41ff981cb9df0f5edeb27751169
David Laptop <cajina@gmail.com>
2020-12-21 10:34:52 -0600

    agregada la función ventas del dia

--- sistema_restaurante_consola/index.js
+++ sistema_restaurante_consola/index.js
@@ -6,10 +6,10 @@ const usuario = {
   deuda: 0
 }
 
-/* variables para pedido del usuario*/
+/* variables*/
 let pedido = []
 let costoPedido = 0
-
+let ventasDia = 0
 
 /** MOSTRAR PRODUCTOS **/
 const mostrarMenu = () => {
@@ -73,8 +73,8 @@ const pedirCuenta = () => {
 const debeAlgo = () => usuario.deuda > 0
 
 //paga pedido
-const pagarPedido = montoEntregado =>{
-  if (debeAlgo) return "No tiene una factura adeudada. Debe hacer un pedido primero."
+const pagarCuenta = montoEntregado =>{
+  if (!debeAlgo) return "No tiene una factura adeudada. Debe hacer un pedido primero."
     switch (true){
       case (montoEntregado < usuario.deuda):
         console.log("No te alcanza para pagar tu factura.");
@@ -82,14 +82,21 @@ const pagarPedido = montoEntregado =>{
 
       case (montoEntregado === usuario.deuda):
         console.log(`Tu pedido ha sido pagado satisfactoriamente.`);
+        ventasDia += usuario.deuda
         usuario.deuda = 0
+        
         break;
 
       case (montoEntregado > usuario.deuda):
-        console.log(`Tu pedido ha sido pagado y tu cambio es de ${montoEntregado - usuario.deuda}`);
+        console.log(`Tu pedido ha sido pagado y tu cambio es de $${montoEntregado - usuario.deuda}`);
+        ventasDia += usuario.deuda
         usuario.deuda = 0
         break;
       default:
         console.log(`Algo salió mal.`);
     }
 }
+
+
+/** VENTAS DEL DIA **/
+const muestraVentasDia = () => console.log(`Las ventas del día han sido de $${ventasDia}`);
