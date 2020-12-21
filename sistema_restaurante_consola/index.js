35c1955d3e129cca613d3aed90e9e593ad063dd3
David Laptop <cajina@gmail.com>
2020-12-21 08:43:45 -0600

    funcion calcular pedido agregada

--- sistema_restaurante_consola/index.js
+++ sistema_restaurante_consola/index.js
@@ -1,6 +1,17 @@
 
-/** MOSTRAR PRODUCTOS **/
+/** USUARIO **/
+const usuario = {
+  nombre: "David",
+  edad: 30,
+  deuda: 0
+}
 
+/* variables para pedido del usuario*/
+const pedido = []
+let costoPedido = 0
+
+
+/** MOSTRAR PRODUCTOS **/
 const mostrarMenu = () => {
 
   console.log(`CÓDIGO  - NOMBRE PRODUCTO - PRECIO`);
@@ -9,4 +20,39 @@ const mostrarMenu = () => {
   for (let producto of productos) {
     console.log(`${producto.codigo} - ${producto.nombre} - $${producto.costo}`)
   }
+}
+
+/** PEDIR PRODUCTO **/
+const pedirProducto = cod => {
+  if (!cod || typeof cod === "boolean" ) return "Ingrese un código válido" /*Verifica si codigo en blanco*/
+
+  // Si encuentra el código, devuelve producto
+  const productoEncontrado = productos.find(producto => producto.codigo === cod )
+  if (!productoEncontrado) return "El producto no existe" //Si undefined(false) return texto.
+  
+  // agregamos producto al pedido del usuario
+  pedido.push(productoEncontrado)
+  console.log(`Se ha agregado ${productoEncontrado.nombre} a su pedido. \nDescripción: `);
+  return mostrarPedido() //retornamos los productos en pedido
+}
+
+
+/** MOSTRAR PEDIDO **/
+const mostrarPedido = () => pedido
+
+
+/** CALCULANDO EL COSTO DEL PEDIDO **/
+const calcularCosto = () =>{
+  let costo = 0
+  for (producto of pedido){
+    costo+= producto.costo 
+  }
+  costoPedido = costo
+
+  return `Su cuenta actual es de $${costoPedido}.`
+}
+
+/** PAGAR PEDIDO **/
+const finalizarPedido = () {
+  
 }
